8a3195099fc0baa026bf18f2bd7cd6b9789a70c0
jnishwanth <jnishwanth@gmail.com>
2021-10-16 11:48:13 +0530

    added echoes after steps

--- archinstall.sh
+++ archinstall.sh
@@ -10,9 +10,11 @@ hostname = "arch"
 additional_pkgs = "git nvim lxde-common lxsession xmonad xmonad-contrib xterm alacritty xorg pulseaudio pulseaudio-alsa alsa-plugins alsa-utils automake autoconf dosfstools mtools basedevel"
 
 ## NTP
+echo "Setting NTP..."
 timedatectl set-ntp true
 
 ## Formatting and mounting
+echo "Formatting partitions..."
 mkfs.ext4 /dev/$root_part
 mkswap /dev/$swap_part
 
@@ -20,19 +22,24 @@ mount /dev/$root_part /mnt
 swpaon /dev/$swap_part
 
 ## Install base on mnt
+echo "Installing base packages..."
 pacstrap /mnt $base_pkgs
 
 ## Generate filesystem tab
+echo "Generating fstab file..."
 genfstab -U /mnt >> /mnt/etc/fstab
 
 ## Chroot into mnt
+echo "Chrooting into install..."
 arch-chroot /mnt
 
 ## Link your timezone and sync hwclock
+echo "Setting timezone and clock..."
 ln -sf /usr/share/zoneinfo/$timezone /etc/localtime
 hwclock --systohc
 
 ## Generate locale
+echo "Setting up locale config..."
 sed -i -e "s/#en_US.UTF/en_US.UTF/g" /etc/locale.gen
 sed -i -e "s/#en_IN/en_IN/g" /etc/locale.gen
 locale-gen
@@ -40,6 +47,7 @@ locale-gen
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 
 ## Set hostname and hosts file
+echo "Setting hostname and hosts file..."
 echo $hostname >> /etc/hostname
 
 echo "127.0.0.1 localhost" >> /etc/hosts
@@ -47,4 +55,7 @@ echo "::1       localhost" >> /etc/hosts
 echo "127.0.1.1 $hostname.localdomain $hostname"
 
 ## Installing additional packages
+echo "Installing additional packages..."
 pacman -S $additional_pkgs
+
+echo "Done"
