b8525011b37ae76bea4dae3c72c1f257565197a3
jnishwanth <jnishwanth@gmail.com>
2021-10-17 21:02:38 +0530

    bugs

--- postscript.sh
+++ postscript.sh
@@ -31,7 +31,7 @@ echo $hostname >> /etc/hostname
 
 echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1       localhost" >> /etc/hosts
-echo "127.0.1.1 $hostname.localdomain $hostname"
+echo "127.0.1.1 $hostname.localdomain $hostname" >> /etc/hosts
 read a
 
 ## Additional packages
